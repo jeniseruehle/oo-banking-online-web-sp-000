19f09a3d0e90ab728cec83365e47a3f3e3df4ea3
Jenise Ruehle <jela07@gmail.com>
2020-03-22 20:28:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,13 @@ class Transfer
   end 
   
   def execute_transaction
-    if valid? && @status == "pending"
+    if sender.balance < @amount 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    else
+      @sender.deposit(amount*-1)
+      @receiver.deposit(@amount)
+      @status = "complete"
     end 
   end 
   
