f9d8380cb4c27b2db11d829feda839804fcf73d4
Jenise Ruehle <jela07@gmail.com>
2020-03-22 20:44:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,11 +9,11 @@ class Transfer
   end 
   
   def valid?
-    sender.valid? && receiver.valid?
+    @sender.valid? && @receiver.valid?
   end 
   
   def execute_transaction
-    if sender.balance < amount
+    if @sender.balance < amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     elsif @status != "complete" && @sender.valid?
