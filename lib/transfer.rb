08cb5c7b35bee90964af162f191dd735461904ed
Jenise Ruehle <jela07@gmail.com>
2020-03-22 20:35:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     elsif
-      !@sender.valid? || @sender.balance < @amount
+      !BankAccount.valid? || @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     else
