f2637a208f9c9a7a00973ee2d27d20c6327077ea
Jenise Ruehle <jela07@gmail.com>
2020-03-22 20:13:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+  
+  def initialize
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @transfer_amount
+  end 
+  
 end
